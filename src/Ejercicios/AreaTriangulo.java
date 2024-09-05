f1c7ac11eff734f822632586a12af8accd94d827
jptsoft <juan.delvillar@tsoftglobal.com>
2024-09-05 15:21:10 -0400

    se resuelve ejercicio 7

---
+++ src/Ejercicios/AreaTriangulo.java
@@ -0,0 +1,18 @@
+package Ejercicios;
+
+public class AreaTriangulo {
+    /*
+    * 7. Área de un triángulo
+    * Descripción: Dadas la base b = 5 y
+    *                   la altura h = 8 de un triángulo,
+    * calcula su área usando la fórmula:
+    * Variables: b = 5, h = 8.
+    */
+
+    public static void main(String[] args) {
+        int b = 5;
+        int h = 8;
+        double area= (double) (b * h) /2;
+        System.out.println("area = " + area);
+    }
+}
