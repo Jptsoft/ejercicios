ec9ea658757ced177dcde971d743d5e43644a82e
jptsoft <juan.delvillar@tsoftglobal.com>
2024-09-09 15:07:18 -0300

    se resuelve ejercicio 21

---
+++ src/Ejercicios/RaizCuadradaSinLibrerias.java
@@ -0,0 +1,17 @@
+package Ejercicios;
+
+public class RaizCuadradaSinLibrerias {
+    /*
+    * Raíz cuadrada sin usar librerías
+    * Descripción: Calcula la raíz cuadrada del número a = 49
+    * sin usar funciones predefinidas.
+    * La raíz cuadrada de 49 es 7.
+    * Variables: a = 49.
+    */
+
+    public static void main(String[] args) {
+        double numero = 49;
+        double raizCuadrada = Math.sqrt(numero);
+        System.out.println("La raíz cuadrada de " + numero + " es: " + raizCuadrada);
+    }
+}
